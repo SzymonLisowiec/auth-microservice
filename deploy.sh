4c7f06bebc294555735f3b4eb14040b10cd80f1d
Kysune <szymonlisowiec@gmail.com>
2019-05-05 12:10:39 +0200

    Added deploy.sh for deploying github page

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # abort on errors
-
+set -e
 
 # build
 npm run docs:build
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -u -f https://github.com/SzymonLisowiec/auth-microservice-page master
+git push -u -f https://github.com/SzymonLisowiec/auth-microservice master:gh-pages
 
 cd -
